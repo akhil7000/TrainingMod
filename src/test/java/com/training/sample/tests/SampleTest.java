71427e1408bb9d95d9b51eaa9fce8c1a32aa062d
shrikanttechincal <shrikantshetty5@gmail.com>
2020-09-30 13:05:29 +0530

    flipkart/search

--- src/test/java/com/training/sample/tests/SampleTest.java
+++ src/test/java/com/training/sample/tests/SampleTest.java
@@ -1,6 +1,10 @@
 package com.training.sample.tests;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.training.sample.pages.SamplePage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,12 +15,42 @@ import org.slf4j.LoggerFactory;
 public class SampleTest {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+SamplePage sample=new SamplePage();
+SortingFilter sortfilter=new SortingFilter();
+    @BeforeEach
+    public void init(){
+        Configuration.baseUrl="https://www.flipkart.com";
+        Selenide.open("/");
+    }
 
     @Test
-    public void sampleTest(){
-
-        logger.info("Sample test");
-        SamplePage samplePage=new SamplePage();
-        samplePage.samplePageMethod();
+    public void sampleTest() throws Exception {
+            Assertions.assertTrue(getDetails());
     }
-}
+      public Boolean test1(String product,String filter) throws Exception {
+          product=product.trim();
+          logger.info(product);
+           //to handle popup
+          sample.popUpCancel();
+          //to enter product i.e shoes in textfield
+          sample.setShoes(product);
+          //to enter search button
+          sample.searchShoes();
+          Thread.sleep(5000);
+          //to select low to high filter option
+          sortfilter.sortShoes(filter);//price low to high
+          Thread.sleep(5000);
+          //to do the pagination of page1
+          sortfilter.counting();//counting prices of page1
+
+         // sortfilter.counting();//counting prices of page2
+      return true;
+    }
+
+    public Boolean getDetails() throws Exception {
+        String product ="shoes";
+        String filter="Price -- Low to High";
+        return test1(product,filter);
+    }
+
+    }
---
+++ src/test/java/com/training/sample/tests/SortingFilter.java
@@ -0,0 +1,65 @@
+package com.training.sample.tests;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import com.training.sample.pages.SamplePage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SortingFilter {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    ArrayList a1 = new ArrayList();
+    ArrayList a2 = new ArrayList();
+
+    public void sortShoes(String filter) {
+
+        ElementsCollection sortingfilter = $$x("//div[@class='_3ywJNQ']/div");
+        Iterator<SelenideElement> itTotal = sortingfilter.iterator();
+
+        int count = 0;
+        int position = 0;
+        while (itTotal.hasNext()) {
+            count++;
+            String temp = itTotal.next().getText();
+            temp = temp.trim();
+            if (filter.contains(temp)) {
+                position = count;
+                $x("//div[@class='_3ywJNQ']/div[" + position + "]").click();
+            }
+            logger.info("temp = " + temp);
+
+        }
+        logger.info("position = " + position);
+        logger.info(filter);
+
+    }
+
+
+    public void counting() {
+ElementsCollection priceList = $$x("//div[@class='_1vC4OE']");
+    Iterator<SelenideElement> iterate_priceList = priceList.iterator();
+
+        while(iterate_priceList.hasNext()){
+        SelenideElement tempPriceList = iterate_priceList.next();
+        String singlePriceList = tempPriceList.getText();
+        String price_without_rupees = singlePriceList.split("\u20B9")[1];
+        a1.add(price_without_rupees);
+    }
+        a2=(ArrayList) a1.clone();
+        Collections.sort(a2);
+
+        logger.info("a1 = "+a1);
+        logger.info("a2 = "+a2);
+        logger.info("Size of A1 = "+a1.size());
+        logger.info("Size of A2 = "+a2.size());
+        //return a3.equals(a4);
+
+}
+
+}
