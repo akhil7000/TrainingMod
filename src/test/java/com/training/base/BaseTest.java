5746b02b17c5acbb300fc22eb739c023adfd19e5
Pranav <hanwantepranav@gmail.com>
2020-10-04 00:11:28 +0530

    added soft assertion

--- src/test/java/com/training/base/BaseTest.java
+++ src/test/java/com/training/base/BaseTest.java
@@ -1,18 +1,25 @@
 package com.training.base;
 
 import com.training.utilities.GetJsonValue;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import java.util.Map;
 
 public class BaseTest {
 
     public Map<String, String> map;
-
+    public SoftAssertions softAssert;
     /**
      * Getting json data and storing it in map variable
      */
     @BeforeEach
     public void testSetup(){
+        softAssert = new SoftAssertions();
         map = (Map) new GetJsonValue().getValue();
+    }
+    @AfterEach
+    public void tearDown(){
+        softAssert.assertAll();
     }
 }
