e46955a8395c0c88312d1267e1f4cf4e50c4aa7f
shrikanttechincal <shrikantshetty5@gmail.com>
2020-10-06 00:11:59 +0530

    resolveconflicts

--- src/test/java/com/training/base/BaseTest.java
+++ src/test/java/com/training/base/BaseTest.java
@@ -2,6 +2,8 @@ package com.training.base;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.training.utilities.GetJsonValue;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,6 +12,7 @@ import java.util.Map;
 
 public class BaseTest {
     public static Map<String, String> map;
+    public SoftAssertions softAssert;
     /**
      * Getting json data and storing it in map variable
      */
@@ -20,9 +23,12 @@ public class BaseTest {
         options.addArguments("--incognito");
         options.addArguments("start-maximized");
         Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
-    }
+        map = (Map) new GetJsonValue().getValue();
+        }
+
     @AfterEach
     public void tearDown(){
         Selenide.closeWebDriver();
+        softAssert.assertAll();
     }
 }
