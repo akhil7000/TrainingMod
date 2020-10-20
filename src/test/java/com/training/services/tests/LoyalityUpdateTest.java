9ad22be849261d50e62e500a5b5832478e5eeac7
Pranav <hanwantepranav@gmail.com>
2020-10-20 15:05:20 +0530

    done some changes

--- src/test/java/com/training/services/tests/LoyalityUpdateTest.java
+++ src/test/java/com/training/services/tests/LoyalityUpdateTest.java
@@ -9,17 +9,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LoyalityUpdateTest extends BaseTest {
-    Map<String, Object> headerMap;
+    Map<String, String> headerMap;
     RequestBody requestBodyAuthenticate;
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @BeforeAll
     public void setData() {
@@ -45,6 +42,11 @@ public class LoyalityUpdateTest extends BaseTest {
                         , new Gson().toJson(requestBodyAuthenticate))
                         .as(Response.class);
 
+        /**
+         * puttting access token and account ID, and getting response
+         */
+
+        String loyaltyId = "137529822";
         headerMap.put(map.get("accessToken"), authenticationResponse.getPayload().getAccessToken());
 
         com.training.services.ga.loyalty.RequestBody requestBody = new com.training.services.ga.loyalty.RequestBody();
@@ -53,23 +55,23 @@ public class LoyalityUpdateTest extends BaseTest {
         requestBody.setChannel("web");
         requestBody.setVdsId(authenticationResponse.getPayload().getAccountId());
         requestBody.setLastName("Poole");
-        requestBody.setLoyaltyId("137529822");
+        requestBody.setLoyaltyId(loyaltyId);
         requestBody.setBirthdate("19620802");
 
-        com.training.services.ga.loyalty.Response authenticationResponse2
+        com.training.services.ga.loyalty.Response loyaltyResponse
                 = new RestEngine().getResponsePut(map.get("url_base") + "/v1/guestAccounts/loyalty",
                 headerMap, new Gson().toJson(requestBody)).as(com.training.services.ga.loyalty.Response.class);
 
-        Assertions.assertThat(authenticationResponse2.getStatus()).isEqualTo(200)
+        Assertions.assertThat(loyaltyResponse.getStatus()).isEqualTo(200)
                 .as("Json response is not 200");
 
-        softAssert.assertThat(authenticationResponse2.getPayload().getLoyaltyId()).isEqualTo("137529822")
+        softAssert.assertThat(loyaltyResponse.getPayload().getLoyaltyId()).isEqualTo(loyaltyId)
                 .as("Loyalty id is not matching");
 
-        softAssert.assertThat(authenticationResponse2.getPayload().getLoyaltyTier()).isEqualTo("Diamond")
+        softAssert.assertThat(loyaltyResponse.getPayload().getLoyaltyTier()).isEqualTo("Diamond")
                 .as("Guest loyalty tier is not diamond");
 
-        softAssert.assertThat(authenticationResponse2.getPayload().getRelationshipPoints()).isEqualTo("83")
+        softAssert.assertThat(loyaltyResponse.getPayload().getRelationshipPoints()).isEqualTo("83")
                 .as("Relationship points is not equal to 83");
     }
 }
