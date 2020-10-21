b6c43f0f853925bde5294d62f03be48e12aabdbe
shrikanttechincal <shrikantshetty5@gmail.com>
2020-10-21 14:28:27 +0530

    added more assertions

--- src/main/java/com/training/utilities/UniqueMailId.java
+++ src/main/java/com/training/utilities/UniqueMailId.java
@@ -3,7 +3,7 @@ package com.training.utilities;
 import java.util.Date;
 
 public class GetUniqueMailId {
-    public String getUniqueMailId() {
+    public static String getUniqueMailId() {
         Date date = new Date();
         Long luid = (Long) date.getTime();
         String mailid = "test"+luid.toString()+"@api.com";
