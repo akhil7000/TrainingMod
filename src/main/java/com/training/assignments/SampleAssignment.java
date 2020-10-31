0eb58fbb5dc4082334ab1d997c19ea425e605476
“Akhil <acherian@rccl.com>
2020-10-31 12:28:38 +0530

    added sample contents

---
+++ src/main/java/com/training/assignments/SampleAssignment.java
@@ -0,0 +1,18 @@
+package com.training.assignments;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This is a sample Page class
+ */
+public class SampleAssignment {
+
+    private static Logger logger = LoggerFactory.getLogger(SampleAssignment.class.getName());
+
+    public static void main(String[] args){
+
+        logger.info("This is a sample assignment");
+
+    }
+}
