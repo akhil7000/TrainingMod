f2b9df3e5b276c34c5cf01433e2c7133d047db82
shrikanttechincal <shrikantshetty5@gmail.com>
2020-11-02 13:59:47 +0530

    need to work

--- src/test/java/com/training/assignment/tests/AssignmentTest.java
+++ src/test/java/com/training/assignment/tests/AssignmentTest.java
@@ -5,18 +5,30 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AssignmentTest {
+public class AssignmentTest extends BaseTest{
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     Assignment assignment = new Assignment();
 
     @Test
     public void testDataConversion() {
         logger.info("double from int->" + assignment.getDoubleFromInt(55));
+        softAssert.assertThat(assignment.getDoubleFromInt(55)).isEqualTo(55.0)
+                .as("int number not converted to double");
         logger.info("int from double->" + assignment.getIntFromDouble(55.0));
+       softAssert.assertThat(assignment.getIntFromDouble(55.0)).isEqualTo(55)
+                .as("double number not converted to int");
         logger.info("float from double->" + assignment.getFloatFromDouble(55.0555558888888888888));
+        softAssert.assertThat(assignment.getFloatFromDouble(55.0555558888888888888)).isEqualTo(55.055557f)
+                .as("double number not converted to float");
         logger.info("double from float->" + assignment.getDoubleFromFloat((float) 55.00589001231313450));
+        softAssert.assertThat(assignment.getDoubleFromFloat((float) 55.00589001231313450)).isEqualTo(55.005889892578125)
+                .as("float number not converted to double");
         logger.info("byte from long->" + assignment.getByteFromLong(11005987L));
+        softAssert.assertThat(assignment.getByteFromLong(11005987L)).isEqualTo(35(Byte@23))
+                .as("long number not converted to byte");
         logger.info("long from byte->" + assignment.getLongFromByte((byte) 32));
+        softAssert.assertThat(assignment.getLongFromByte((byte) 32)).isEqualTo(32)
+                .as("byte number not converted to long");
     }
 
     @Test
@@ -25,7 +37,10 @@ public class AssignmentTest {
         for (char characters : getcharacter) {
             logger.info("converted in character format-->" + characters);
         }
-        logger.info("converted ram to sam-->" + assignment.getReplacedString("Ram", "Ram", "Sam"));
+        softAssert.assertThat(assignment.getCharacterFromString("rccl")).contains('q','p','y','z')
+                .as("doesn't contains r,c,c,l characters");
+        logger.info("converted ram to sam-->" +
+                assignment.getReplacedString("Ram", "Ram", "Sam"));
         logger.info("converted Tendulkar to Masterrrr-->" +
                 assignment.getReplacedString("Sachin Tendulkar", "Tendulkar", "Masterrrr"));
         logger.info("converted ram to sam-->" +
