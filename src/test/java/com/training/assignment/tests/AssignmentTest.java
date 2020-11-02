c6412b3bb654ca22f7e855d81f4e12f9941da1cd
shrikanttechincal <shrikantshetty5@gmail.com>
2020-11-02 11:25:18 +0530

    after changes

--- src/test/java/com/training/assignment/tests/AssignmentTest.java
+++ src/test/java/com/training/assignment/tests/AssignmentTest.java
@@ -5,38 +5,37 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class AssignmentTest {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-    Assignment assignment=new Assignment();
+    Assignment assignment = new Assignment();
 
     @Test
-    public void  testDataConversion(){
-        logger.info("double from int->"+assignment.getDoubleFromInt(55));
-        logger.info("int from double->"+assignment.getIntFromDouble(55.0));
-        logger.info("float from double->"+assignment.getFloatFromDouble(55.0555558888888888888));
-        logger.info("double from float->"+assignment.getDoubleFromFloat((float)55.00589001231313450));
-        logger.info("byte from long->"+assignment.getByteFromLong(11005987L));
-        logger.info("long from byte->"+assignment.getLongFromByte((byte)32));
+    public void testDataConversion() {
+        logger.info("double from int->" + assignment.getDoubleFromInt(55));
+        logger.info("int from double->" + assignment.getIntFromDouble(55.0));
+        logger.info("float from double->" + assignment.getFloatFromDouble(55.0555558888888888888));
+        logger.info("double from float->" + assignment.getDoubleFromFloat((float) 55.00589001231313450));
+        logger.info("byte from long->" + assignment.getByteFromLong(11005987L));
+        logger.info("long from byte->" + assignment.getLongFromByte((byte) 32));
     }
 
     @Test
-    public void testOperationString(){
-        char[] character = assignment.getCharacterFromString("rccl");
-        for(char afterIteratecharacters:character){
-            logger.info("converted String to character-->"+afterIteratecharacters);
+    public void testOperationString() {
+        char[] getcharacter = assignment.getCharacterFromString("rccl");
+        for (char characters : getcharacter) {
+            logger.info("converted in character format-->" + characters);
         }
-        logger.info("converted ram to sam-->"+assignment.getReplacedString("Ram","Ram","Sam"));
-        logger.info("converted Tendulkar to Masterrrr-->"+
-                assignment.getReplacedString("Sachin Tendulkar","Tendulkar","Masterrrr"));
-        logger.info("converted ram to sam-->"+
-                assignment.getReplacedString("Dell","Dell","Depp"));
-        logger.info("index of X is-->"+assignment.getIndexOf("Xioami",'X'));
-        logger.info("index of a is-->"+assignment.getIndexOf("Xioami",'a'));
-        logger.info("index of o is-->"+assignment.getIndexOf("Xioami",'o'));
-        logger.info("after using subString name-->"+
-                assignment.getNameUsingSubString("Indian Oil Corporation Ltd",11,22));
-        String[] nameArray= {"Paulo Dybala","Federico","Gianluigi","Ronaldo","Messi"};
-        logger.info("index value-->"+assignment.getIndex(nameArray,"Ronaldo"));
+        logger.info("converted ram to sam-->" + assignment.getReplacedString("Ram", "Ram", "Sam"));
+        logger.info("converted Tendulkar to Masterrrr-->" +
+                assignment.getReplacedString("Sachin Tendulkar", "Tendulkar", "Masterrrr"));
+        logger.info("converted ram to sam-->" +
+                assignment.getReplacedString("Dell", "Dell", "Depp"));
+        logger.info("index of X is-->" + assignment.getIndexOf("Xioami", 'X'));
+        logger.info("index of a is-->" + assignment.getIndexOf("Xioami", 'a'));
+        logger.info("index of o is-->" + assignment.getIndexOf("Xioami", 'o'));
+        logger.info("after using subString name-->" +
+                assignment.getNameUsingSubString("Indian Oil Corporation Ltd", 11, 22));
+        logger.info("index value-->" + assignment.
+                getIndex(new String[]{"Paulo Dybala", "Federico", "Gianluigi", "Ronaldo", "Messi"}, "Ronaldo"));
     }
 }
