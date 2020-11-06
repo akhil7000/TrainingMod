81a6de67f623238e0c864e46d591cd785a2940a8
“Akhil <acherian@rccl.com>
2020-11-06 19:18:04 +0530

    adding code for streams api 3

---
+++ src/test/java/com/training/streams/test/SampleTest.java
@@ -0,0 +1,18 @@
+package com.training.streams.test;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This is a sample test using Junit5
+ */
+public class SampleTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    public void sampleTest(){
+        logger.info("Sample test");
+    }
+}
