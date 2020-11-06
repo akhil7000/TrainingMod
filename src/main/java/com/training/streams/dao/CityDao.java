828468d88d44e339e461d81cb52251922a1d4bd5
“Akhil <acherian@rccl.com>
2020-11-06 19:02:12 +0530

    added classes for streams excercise

---
+++ src/main/java/com/training/streams/dao/CityDao.java
@@ -0,0 +1,23 @@
+package com.training.streams.dao;
+
+import java.util.List;
+
+import com.training.streams.domain.City;
+
+/**
+ *
+ * @author Binnur Kurt (binnur.kurt@gmail.com)
+ */
+public interface CityDao {
+    City findCityById(int id);
+
+    City removeCity(City city);
+
+    City addCity(City city);
+
+    City updateCity(City city);
+
+    List<City> findAllCities();
+
+    List<City> findCitiesByCountryCode(String countryCode);
+}
---
+++ src/main/java/com/training/streams/dao/WorldDao.java
@@ -0,0 +1,9 @@
+package com.training.streams.dao;
+
+/**
+ *
+ * @author Binnur Kurt (binnur.kurt@gmail.com)
+ */
+public interface WorldDao extends CountryDao,CityDao {
+
+}
