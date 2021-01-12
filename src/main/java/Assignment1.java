d7ef5f5d78a355e5f57eb6ec8e330365e9b46909
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-12 20:41:51 +0530

    "Assignment1"

--- src/main/java/Assignment1.java
+++ src/main/java/Assignment1.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Assignment1 {
+public class Assignment1 {
+    public static void main(String[] args) {
+        //int to double
+        int i = 100;
+        double d = i;
+
+        //double to int
+        double x =5;
+        int y = (int) x;
+
+        //double to float
+        double a =1000;
+        float b = (float) a;
+
+        // float to double
+        float l = 100.5F;
+        double m = l;
+
+        //byte to long
+        byte p = 1;
+        long q = p;
+
+        //long to byte
+        long r = 21546;
+        byte s = (byte) r;
+
+        //string "rccl" to char[] name
+        String name = "rccl";
+        char [] cname = new char [name.length()];
+        for (i=0;i<name.length();i++) {
+            cname[i]=name.charAt(i);
+            System.out.println(cname[i]);
+        }
+        System.out.println(cname);
+
+
+    }
 }
