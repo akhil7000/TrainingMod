7710b02f8b8096f099c936980355483f0f2eea13
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-12 21:19:17 +0530

    Assignment2

---
+++ src/main/java/Assignment2.java
@@ -0,0 +1,19 @@
+public class Assignment2 {
+    public static void main(String[] args){
+        // Replace Ram to Sam
+        String a= "Ram";
+        a = a.replace('R','S');
+        System.out.println(a);
+
+        // Replace Sachin Tendulkar to Sachin Master
+        String sachin = "Sachin Tendulkar";
+
+        sachin = sachin.replace("Tendulkar","Sachin Masterrrr");
+        System.out.println(sachin);
+
+        // Dell to Depp
+        String d="Dell";
+        d = d.replace("ll","pp");
+        System.out.println(d);
+    }
+}
