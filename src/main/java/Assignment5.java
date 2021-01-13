b8030aa90d28de1fa22490b87548187a48dbc15b
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-13 11:38:29 +0530

    Assignment5 first commit

---
+++ src/main/java/Assignment5.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Assignment5 {
+    public static void main(String[] args) {
+        //Find the index of Ronaldo in the given array
+        //String [] name= {"Paulo Dybala", "Federico","Gianluigi","Ronaldo","Messi"};
+        String [] name = {"Paulo Dybala","Federico","Gianluigi","Ronaldo","Messi"};
+        System.out.println("Index of Ronaldo is: " + Arrays.asList(name).indexOf("Ronaldo"));
+    }
+}
