eda8a2eb6697f17f0bbb8407ece97c5b17b00100
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-13 11:19:52 +0530

    Assignment 3 first commit

---
+++ src/main/java/Assignment3.java
@@ -0,0 +1,10 @@
+public class Assignment3 {
+    public static void main(String[] args) {
+        //Find the index of x,a,o in the given name, string name = “Xioami”
+        String mi = "Xioami";
+        System.out.println("Index of X is " + mi.indexOf('X'));
+        System.out.println("Index of a is " + mi.indexOf('a'));
+        System.out.println("Index of o is " + mi.indexOf('o'));
+
+    }
+}
