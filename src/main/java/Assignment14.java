ccb0725d01e361e3bbc92b1e1140b2e3c6372524
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-14 11:19:37 +0530

    Assignment14 2nd commit

--- src/main/java/Assignment14.java
+++ src/main/java/Assignment14.java
@@ -1,18 +1,16 @@
 class Actor{
-    public void work(){
+public void work(){
         System.out.println("Doing Acting");
     }
 }
 class Director {
-    public void work(){
+public void work(){
         System.out.println("Directing");
     }
 }
 public class Assignment14 {
-    public static void main(String[]args){
-        Actor act = new Actor();
-        Director dir = new Director();
-        act.work();
-        dir.work();
-    }
+public static void main(String[]args){
+    new Actor().work();
+    new Director().work();
+}
 }
