795f36821295069776df700386eed52800783f7b
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-14 12:16:13 +0530

    test

---
+++ src/main/java/com/assignment/Assignment9.java
@@ -0,0 +1,14 @@
+package com.assignment;
+public class Assignment9 {
+    public static void evenNumber() {
+    int i = 1;
+    while (i <= 500) {
+        if (i % 2 == 0) {
+            System.out.println(i);
+        }
+        i++;
+        }
+    }
+}
+
+
---
+++ src/test/java/com/assignmentTest/assignment9Test.java
@@ -0,0 +1,11 @@
+package com.assignmentTest;
+import com.assignment.Assignment9;
+import org.junit.jupiter.api.Test;
+
+public class assignment9Test {
+
+    @Test
+    public void printEvenNumbers(){
+    Assignment9.evenNumber();
+    }
+}
