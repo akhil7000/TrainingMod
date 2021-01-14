9858d979f4095c9f0a2c73bd774b2138a554daed
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-14 11:29:50 +0530

    Assignment11 2nd commit

--- src/main/java/Assignment11.java
+++ src/main/java/Assignment11.java
@@ -1,11 +1,11 @@
 public class Assignment11 {
-    public static void main(String[] args) {
-        String sport[]={"Tennis", "Volleyball","Cricket","Cycling","Swimming","Badminton"};
-        for(int i=0;i<sport.length;i++)
-        {
-            if(i!=2){
-                System.out.println(sport[i]);
-            }
-        }
+public static void main(String[] args) {
+    String sport[]={"Tennis", "Volleyball","Cricket","Cycling","Swimming","Badminton"};
+    for(int i=0;i<sport.length;i++)
+    {
+    if(i!=2){
+        System.out.println(sport[i]);
     }
+    }
+}
 }
