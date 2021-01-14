3eac13742125ae6fc40ba22f9c9127f1e075a2b9
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-14 11:13:23 +0530

    Assignment15 commit2

--- src/main/java/Assignment15.java
+++ src/main/java/Assignment15.java
@@ -1,29 +1,22 @@
-class Name{
-
-    static String fullName(String firstName)
-    {
-        return firstName;
-    }
-    static String fullName(String firstName, String lastName)
-    {
-        return firstName +" "+ lastName;
-    }
-    static String fullName(String firstName, String lastName, String middleName)
-    {
-        return firstName + " "+ lastName+" "+ middleName;
-    }
-
+class FullNameOverload{
+static String fullName(String firstName)
+{
+    return firstName;
+}
+static String fullName(String firstName, String lastName)
+{
+    return firstName +" "+ lastName;
+}
+static String fullName(String firstName, String lastName, String middleName)
+{
+return firstName + " "+ lastName+" "+ middleName;
+}
 }
 public class Assignment15 {
-    public static void main(String[] args){
-        Name methodOverload = new Name();
-        String x = methodOverload.fullName("Barack");
-        String y = methodOverload.fullName("Barack","Obama");
-        String z = methodOverload.fullName("Barack","Obama","Hussein");
-        System.out.println("Method 1 "+x);
-        System.out.println("Method 2 "+y);
-        System.out.println("Method 3 "+z);
-
-
-    }
+public static void main(String[] args){
+    FullNameOverload methodOverload = new FullNameOverload();
+    System.out.println("Method 1 "+methodOverload.fullName("Barack"));
+    System.out.println("Method 2 "+methodOverload.fullName("Barack","Obama"));
+    System.out.println("Method 3 "+methodOverload.fullName("Barack","Obama","Hussein"));
+}
 }
