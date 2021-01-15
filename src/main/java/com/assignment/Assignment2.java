c0bd2aea5c9e36b8f4300a31964a0be6c5350f91
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-15 16:33:41 +0530

    test commit

---
+++ src/main/java/com/assignment/Assignment2.java
@@ -0,0 +1,21 @@
+package com.assignment;
+
+public class Assignment2 {
+    public static void stringTest(){
+        // Replace Ram to Sam
+        //String a= "Ram";
+
+        //a = a.replace('R','S');
+        System.out.println("Ram".replace('R','S'));
+
+        // Replace Sachin Tendulkar to Sachin Master
+        //String sachin = "Sachin Tendulkar";
+        //sachin = sachin.replace("Tendulkar","Masterrrr");
+        System.out.println("Sachin Tendulkar".replace("Tendulkar","Masterrrr"));
+
+        // Dell to Depp
+        //String d="Dell";
+        //d = d.replace("ll","pp");
+        System.out.println("Dell".replace("ll","pp"));
+    }
+}
---
+++ src/test/java/com/assignmentTest/assignmentTest.java
@@ -0,0 +1,11 @@
+package com.assignmentTest;
+
+import org.junit.jupiter.api.Test;
+import com.assignment.Assignment2;
+
+public class assignmentTest {
+    @Test
+    public void Test(){
+        Assignment2.stringTest();
+    }
+}
