28bb294f5e185dee7adc8b0b1a8fdd65d4beb46f
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-18 09:45:00 +0530

    Second test commit

--- src/main/java/com/assignment/Assignment8.java
+++ src/main/java/com/assignment/Assignment8.java
@@ -3,12 +3,7 @@ package com.assignment;
 //Printing middle number
 public class Assignment8 {
     public static int middle(){
-        int[] arrayM = new int[500];
-        for(int i=0; i<500; i++) {
-            arrayM[i]=i+1;
-        }
-        int middleNumber = (arrayM.length)/2;
-        return middleNumber;
-
+    int[] arrayM = new int[500];
+    return (arrayM.length)/2;
     }
 }
