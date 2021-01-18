61fca41cef4dd73b7d89ef6b3d64e132bd97f3fa
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-18 16:31:38 +0530

    Second Test commit

---
+++ src/main/java/com/assignment/Assignment3.java
@@ -0,0 +1,9 @@
+package com.assignment;
+
+public class Assignment3 {
+    public int getIndex(char a) {
+        //Find the index of x,a,o in the given name, string name = "Xioami"
+        String originalString = "Xioami";
+        return originalString.indexOf(a);
+    }
+}
