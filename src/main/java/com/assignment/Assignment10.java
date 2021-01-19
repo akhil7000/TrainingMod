14451ff6400bd533f0a0bd41d6256d52050251bd
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-19 21:34:39 +0530

    draft

--- src/main/java/com/assignment/Assignment10.java
+++ src/main/java/com/assignment/Assignment10.java
@@ -3,19 +3,13 @@ package com.assignment;
 import java.util.Arrays;
 
 public class Assignment10 {
-    public static void main(String[] args) {
-        int[] intArray = new int[10];
-        for (int i=0;i<10;i++)
-        {
-            intArray[i]=i+1;
-            if (i%2==1){
-                System.out.println(i);
+    public void odd(int min, int max) {
+        int[] intArray = new int[max - min+1];
+        for (int i = min; i < max; i++) {
+            intArray[i] = i;
+            if (i % 2 == 1) {
+            System.out.println(i);
             }
         }
-        /*for (int i : intArray) {
-            intArray[i]=i+1;
-            if (i%2==1){
-                System.out.println(i);
-            }*/
-        }
     }
+}
