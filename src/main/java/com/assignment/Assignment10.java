a09525a2f3fd31e07a94fbada7c7a5a06d698b19
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-19 19:29:19 +0530

    draft

--- src/main/java/com/assignment/Assignment10.java
+++ src/main/java/com/assignment/Assignment10.java
@@ -1,16 +1,21 @@
+package com.assignment;
+
 import java.util.Arrays;
 
 public class Assignment10 {
     public static void main(String[] args) {
-        int[] intArray = new int[20];
+        int[] intArray = new int[10];
         for (int i=0;i<10;i++)
         {
             intArray[i]=i+1;
-        }
-        for (int i : intArray) {
             if (i%2==1){
                 System.out.println(i);
             }
         }
+        /*for (int i : intArray) {
+            intArray[i]=i+1;
+            if (i%2==1){
+                System.out.println(i);
+            }*/
+        }
     }
-}
