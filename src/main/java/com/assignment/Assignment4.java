301c5643e3180f7c504f57e20c3a996d00f8c7da
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-19 14:21:20 +0530

    Logger commit

--- src/main/java/com/assignment/Assignment4.java
+++ src/main/java/com/assignment/Assignment4.java
@@ -1,9 +1,6 @@
 package com.assignment;
 public class Assignment4 {
     public String getSubString(String originalString,String subString) {
-        /*String IOCL = "Indian Oil Corporation Ltd";
-        String Corp = IOCL.substring(IOCL.indexOf('C'),IOCL.lastIndexOf('n')+1);
-        ,subString.charAt(0)+subString.length()*/
         return originalString.substring(originalString.indexOf(subString.charAt(0)),originalString.indexOf(subString.charAt(0))+subString.length());
        }
 }
--- src/test/java/com/assignmentTest/Assignment4Test.java
+++ src/test/java/com/assignmentTest/Assignment4Test.java
@@ -2,12 +2,16 @@ package com.assignmentTest;
 import org.junit.jupiter.api.Assertions;
 import com.assignment.Assignment4;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Assignment4Test {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Test
     public void substring(){
         Assignment4 Test = new Assignment4();
-        System.out.println(Test.getSubString("Indian Oil Corporation Ltd","Corporation"));
-        Assertions.assertEquals(Test.getSubString("Indian Oil Corporation Ltd","Corporation"),"Corporation");
+        logger.info(Test.getSubString("Indian Oil Corporation Ltd","Corporation"));
+        Assertions.assertEquals(Test.getSubString("Indian Oil Corporation Ltd","Corporation"),"Corporation","String doesn't match");
     }
 }
