32b437e29e840b8111c0579910a6186dd71de5b8
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-19 21:36:18 +0530

    draft

---
+++ src/test/java/com/assignmentTest/Assignment10Test.java
@@ -0,0 +1,13 @@
+package com.assignmentTest;
+import  com.assignment.Assignment10;
+import org.junit.jupiter.api.Test;
+
+public class Assignment10Test {
+
+        @Test
+        public void printodd(){
+        Assignment10 assignment10=new Assignment10();
+        assignment10.odd(1,20);
+        }
+
+}
