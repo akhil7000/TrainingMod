d0d824ed7c68c8b8ace2f9d0bce7f0f88fc2220a
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-19 12:36:59 +0530

    Logger commit

--- src/test/java/com/assignmentTest/Assignment3Test.java
+++ src/test/java/com/assignmentTest/Assignment3Test.java
@@ -3,17 +3,18 @@ package com.assignmentTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import com.assignment.Assignment3;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 public class Assignment3Test {
-
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Test
     public void getCharacterIndex(){
     Assignment3 indexCharacter=new Assignment3();
-    System.out.println(indexCharacter.getIndex('X'));
-    System.out.println(indexCharacter.getIndex('a'));
-    System.out.println(indexCharacter.getIndex('o'));
+    logger.info(Integer.toString(indexCharacter.getIndex('X')));
+    logger.info(Integer.toString(indexCharacter.getIndex('a')));
+    logger.info(Integer.toString(indexCharacter.getIndex('o')));
     Assertions.assertEquals(indexCharacter.getIndex('X'),0);
     Assertions.assertEquals(indexCharacter.getIndex('a'),3);
     Assertions.assertEquals(indexCharacter.getIndex('o'),2);
-
     }
 }
