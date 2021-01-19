9de20bfc9c061c55913f0011142ececb5e09f84f
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-19 12:08:41 +0530

    Logger commit

--- src/test/java/com/assignmentTest/assignment2Test.java
+++ src/test/java/com/assignmentTest/assignment2Test.java
@@ -3,18 +3,19 @@ package com.assignmentTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import com.assignment.Assignment2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import sun.jvm.hotspot.utilities.Assert;
 
 public class assignment2Test {
-
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Test
     public void stringReplacement(){
-        System.out.println(Assignment2.getReplacedSubString("Ram","R","S"));
+        logger.info(Assignment2.getReplacedSubString("Ram","R","S"));
+        logger.info(Assignment2.getReplacedSubString("Sachin Tendulkar","Tendulkar","Masterrrr"));
+        logger.info(Assignment2.getReplacedSubString("Dell","ll","pp"));
         Assertions.assertEquals(Assignment2.getReplacedSubString("Ram","R","S"),"Sam");
-        System.out.println(Assignment2.getReplacedSubString("Sachin Tendulkar","Tendulkar","Masterrrr"));
         Assertions.assertEquals(Assignment2.getReplacedSubString("Sachin Tendulkar","Tendulkar","Masterrrr"),"Sachin Masterrrr");
-        System.out.println(Assignment2.getReplacedSubString("Dell","ll","pp"));
         Assertions.assertEquals(Assignment2.getReplacedSubString("Dell","ll","pp"),"Depp");
-
     }
 }
