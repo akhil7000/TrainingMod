73d4b998f0376edb9ac71ec3aacc5c15ca70b94f
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-20 19:10:39 +0530

    commit 8

--- src/main/java/com/assignment/Assignment5.java
+++ src/main/java/com/assignment/Assignment5.java
@@ -3,6 +3,7 @@ package com.assignment;
 import java.util.Arrays;
 
 public class Assignment5 {
+
     public int getPlayerIndex(String[] players, String indexString) {
         return Arrays.asList(players).indexOf(indexString);
     }
