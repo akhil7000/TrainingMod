895e885ef50841022bb7823f0f32e579aa15c1db
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-20 18:46:10 +0530

    draft

---
+++ src/test/java/com/assignmentTest/Assignment1Test.java
@@ -0,0 +1,11 @@
+package com.assignmentTest;
+
+import org.junit.jupiter.api.Test;
+
+public class Assignment1Test {
+
+    @Test
+    public void testCovertDataType{
+        getConvertedDatatype
+    }
+}
