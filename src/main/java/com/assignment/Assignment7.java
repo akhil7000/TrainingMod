1b4ba0c633e3e1792034acd4af3e97cb6929fc16
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-20 16:38:24 +0530

    commit 6

--- src/main/java/com/assignment/Assignment7.java
+++ src/main/java/com/assignment/Assignment7.java
@@ -5,15 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Assignment7 {
-    public String return_where_user_will_Go(String quality) {
-    if (quality.equals("very good")) {
-    return  "Go to heaven with dogs";
-    }
-    else if (quality.equals("good")) {
-    return "Go to heaven";
-    }
-    else {
-    return "Go to hell";
-    }
+
+    public String getWhereUserWillGo(String quality) {
+        if (quality.equals("very good")) {
+            return "Go to heaven with dogs";
+        } else if (quality.equals("good")) {
+            return "Go to heaven";
+        } else {
+            return "Go to hell";
+        }
     }
 }
--- src/test/java/com/assignmentTest/Assignment7Test.java
+++ src/test/java/com/assignmentTest/Assignment7Test.java
@@ -1,4 +1,5 @@
 package com.assignmentTest;
+
 import com.assignment.Assignment7;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,10 +10,10 @@ public class Assignment7Test {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Test
+    public void testWillGoToHeavenOrHell() {
+        Assignment7 assignment7 = new Assignment7();
 
-    public void will_go_to_heaven_or_hell(){
-    Assignment7 assignment7 =new Assignment7();
-    logger.info(assignment7.return_where_user_will_Go("very good"));
-    Assertions.assertEquals(assignment7.return_where_user_will_Go("very good"),"Go to heaven with dogs","Please enter proper parameters");
+        logger.info(assignment7.getWhereUserWillGo("very good"));
+        Assertions.assertEquals(assignment7.getWhereUserWillGo("very good"), "Go to heaven with dogs", "Please enter proper parameters");
     }
 }
