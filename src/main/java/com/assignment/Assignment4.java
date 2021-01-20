de88d179508af03d4920062fdb0f997360f614e2
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-20 16:02:20 +0530

    commit 10

--- src/main/java/com/assignment/Assignment4.java
+++ src/main/java/com/assignment/Assignment4.java
@@ -2,7 +2,7 @@ package com.assignment;
 
 public class Assignment4 {
 
-    public String returnSubString(String originalString,String subString) {
-        return originalString.substring(originalString.indexOf(subString.charAt(0)),originalString.indexOf(subString.charAt(0))+subString.length());
-       }
+    public String getSubString(String originalString, String subString) {
+        return originalString.substring(originalString.indexOf(subString.charAt(0)), originalString.indexOf(subString.charAt(0)) + subString.length());
+    }
 }
