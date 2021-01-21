256c691fd7d779a313bf52d9eb627be497b07e94
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-21 19:32:06 +0530

    draft

--- src/main/java/com/assignment/Assignment10.java
+++ src/main/java/com/assignment/Assignment10.java
@@ -1,15 +1,15 @@
 package com.assignment;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Assignment10 {
-    public void odd(int min, int max) {
-        int[] intArray = new int[max - min+1];
-        for (int i = min; i < max; i++) {
-            intArray[i] = i;
+    public ArrayList getOddNumber(int min, int max){
+        ArrayList<Integer> oddNumbers = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
             if (i % 2 == 1) {
-            System.out.println(i);
+                oddNumbers.add(i);
             }
         }
+    return oddNumbers;
     }
 }
--- src/test/java/com/assignmentTest/Assignment10Test.java
+++ src/test/java/com/assignmentTest/Assignment10Test.java
@@ -1,13 +1,24 @@
 package com.assignmentTest;
-import  com.assignment.Assignment10;
+
+import com.assignment.Assignment10;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Assignment10Test {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-        @Test
-        public void printodd(){
-        Assignment10 assignment10=new Assignment10();
-        assignment10.odd(1,20);
+    @Test
+    public void printodd() {
+        Assignment10 assignment10 = new Assignment10();
+
+        ArrayList abc = assignment10.getOddNumber(10, 20);
+        Iterator itr = abc.iterator();
+        while (itr.hasNext()) {
+            logger.info((itr.next()).toString());
         }
-
+    }
 }
