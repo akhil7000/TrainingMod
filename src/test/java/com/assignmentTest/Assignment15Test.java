4075b9ce6e090750447a24076e071905ac8af042
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-21 19:38:36 +0530

    commit 4

--- src/test/java/com/assignmentTest/Assignment15Test.java
+++ src/test/java/com/assignmentTest/Assignment15Test.java
@@ -1,6 +1,7 @@
 package com.assignmentTest;
 
 import com.assignment.Assignment15;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,9 +11,16 @@ public class Assignment15Test {
 
     @Test
     public void testGetFullName() {
+
         Assignment15 assignment15 = new Assignment15();
         logger.info(assignment15.fullName("Barrack"));
+
+        Assertions.assertEquals(assignment15.fullName("Barrack"),"Barrack","Not full name");
         logger.info(assignment15.fullName("Barrack", "Obama"));
+        Assertions.assertEquals(assignment15.fullName("Barrack", "Obama"),"Barrack Obama", "Not full name");
+
         logger.info(assignment15.fullName("Barrack", "Hussain", "Obama"));
+        Assertions.assertEquals(assignment15.fullName("Barrack", "Hussain", "Obama"),
+                "Barrack Hussain Obama","Not full name");
     }
 }
