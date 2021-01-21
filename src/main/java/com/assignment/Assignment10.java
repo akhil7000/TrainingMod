8afed36e472aa7ee9d7945da22f10691fe737d31
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-21 21:22:44 +0530

    draft

--- src/main/java/com/assignment/Assignment10.java
+++ src/main/java/com/assignment/Assignment10.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 
 public class Assignment10 {
     public ArrayList getOddNumber(int min, int max){
-        ArrayList<Integer> oddNumbers = new ArrayList<>();
+
+        ArrayList<Integer> oddNumbersArraylist = new ArrayList<>();
         for (int i = min; i <= max; i++) {
             if (i % 2 == 1) {
-                oddNumbers.add(i);
+                oddNumbersArraylist.add(i);
             }
         }
-    return oddNumbers;
+    return oddNumbersArraylist;
     }
 }
--- src/test/java/com/assignmentTest/Assignment10Test.java
+++ src/test/java/com/assignmentTest/Assignment10Test.java
@@ -1,6 +1,7 @@
 package com.assignmentTest;
 
 import com.assignment.Assignment10;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +15,21 @@ public class Assignment10Test {
     @Test
     public void printodd() {
         Assignment10 assignment10 = new Assignment10();
-
-        ArrayList abc = assignment10.getOddNumber(10, 20);
-        Iterator itr = abc.iterator();
-        while (itr.hasNext()) {
-            logger.info((itr.next()).toString());
+        int min = 1;
+        int max = 10;
+        ArrayList oddNumbers = assignment10.getOddNumber(min, max);
+        Iterator iterator = oddNumbers.iterator();
+        while (iterator.hasNext()) {
+            logger.info((iterator.next()).toString());
         }
+
+        ArrayList<Integer> oddNumbersVerfication = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
+            if (i % 2 == 1) {
+                oddNumbersVerfication.add(i);
+            }
+        }
+
+        Assertions.assertArrayEquals(oddNumbers.toArray(), oddNumbersVerfication.toArray(), "Doesnt Match");
     }
 }
