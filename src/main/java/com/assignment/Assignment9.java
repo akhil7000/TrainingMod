6c2ca8dfb0898c7a113dba4f79f1ed944ea0350c
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-22 15:49:04 +0530

    commit 3

--- src/main/java/com/assignment/Assignment9.java
+++ src/main/java/com/assignment/Assignment9.java
@@ -1,14 +1,18 @@
 package com.assignment;
+
+import java.util.ArrayList;
+
 public class Assignment9 {
-    public static void evenNumber() {
-    int i = 1;
-    while (i <= 500) {
-        if (i % 2 == 0) {
-            System.out.println(i);
+
+    public ArrayList getEvenNumber(int min, int max) {
+        int i = min;
+        ArrayList<Integer> evenNumberArray = new ArrayList<>();
+        while (i <= max) {
+            if (i % 2 == 0) {
+                evenNumberArray.add(i);
+            }
+            i++;
         }
-        i++;
-        }
+        return evenNumberArray;
     }
 }
-
-
--- src/test/java/com/assignmentTest/assignment9Test.java
+++ src/test/java/com/assignmentTest/assignment9Test.java
@@ -1,11 +1,37 @@
 package com.assignmentTest;
+
 import com.assignment.Assignment9;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class assignment9Test {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Test
-    public void printEvenNumbers(){
-    Assignment9.evenNumber();
+    public void testEvenNumbers() {
+        int min = 1;
+        int max = 500;
+        Assignment9 assignment9 = new Assignment9();
+
+        ArrayList evenNumbers = assignment9.getEvenNumber(min, max);
+        Iterator iterator = evenNumbers.iterator();
+        while (iterator.hasNext()) {
+            logger.info((iterator.next()).toString());
+        }
+
+        //Assertion to check if Array is correct
+        ArrayList<Integer> evenNumbersVerfication = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
+            if (i % 2 == 0) {
+                evenNumbersVerfication.add(i);
+            }
+        }
+
+        Assertions.assertArrayEquals(evenNumbers.toArray(), evenNumbersVerfication.toArray(), "Doesn't Match");
     }
 }
