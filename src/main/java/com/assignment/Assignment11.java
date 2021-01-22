87ba84ed4f6f376e29bc6177d63fc5e6ff221161
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-22 18:56:13 +0530

    draft

--- src/main/java/com/assignment/Assignment11.java
+++ src/main/java/com/assignment/Assignment11.java
@@ -1,10 +1,14 @@
 package com.assignment;
 
+import java.util.ArrayList;
+
 public class Assignment11 {
-            for(int i=0;i<sport.length;i++)
-    {
-        if(i!=2){
-            System.out.println(sport[i]);
-        }
+    public ArrayList getSportExceptAtIndex(ArrayList sport, int index){
+        ArrayList<String> newSportArray=new ArrayList<>();
+        for(int i=0;i<sport.size();i++)
+            if(i!=index){
+                newSportArray.add(String.valueOf(sport.get(i)));
+            }
+        return newSportArray;
     }
 }
--- src/test/java/com/assignmentTest/Assignment11Test.java
+++ src/test/java/com/assignmentTest/Assignment11Test.java
@@ -1,4 +1,41 @@
 package com.assignmentTest;
 
+import com.assignment.Assignment11;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class Assignment11Test {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    public void testSportWithoutIndexAt(){
+        ArrayList<String> sportNamesArray=new ArrayList<>();
+        sportNamesArray.addAll(Arrays.asList("Tennis", "Volleyball","Cricket","Cycling","Swimming","Badminton"));
+        int index =3;
+        Assignment11 assignment11=new Assignment11();
+        ArrayList newSportNamesArray = assignment11.getSportExceptAtIndex(sportNamesArray,index);
+        Iterator iterator = newSportNamesArray.iterator();
+
+        while(iterator.hasNext()){
+            logger.info(iterator.next().toString());
+        }
+
+        /**
+         * Assertion to check if correct array has been retrieved
+         */
+        ArrayList <String>sportNamesVerificationArray=new ArrayList<>();
+        for(int i=0;i<sportNamesArray.size();i++){
+            if (i!=index)
+            {
+                sportNamesVerificationArray.add(sportNamesArray.get(i));
+            }
+        }
+        System.out.println(sportNamesVerificationArray);
+
+    }
 }
+
