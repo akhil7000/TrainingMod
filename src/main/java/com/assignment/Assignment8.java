3c11307cb7750afcb672f7544665ac475e4df9dd
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-22 15:51:54 +0530

    commit 7

--- src/main/java/com/assignment/Assignment8.java
+++ src/main/java/com/assignment/Assignment8.java
@@ -3,9 +3,9 @@ package com.assignment;
 public class Assignment8 {
 
     public int getMiddleNumber(int min, int max) {
-        for (int i = min; i <= max; i++) {
-            if (i == (max + min) / 2) {
-                return i;
+        for (int index = min; index <= max; index++) {
+            if (index == (max + min) / 2) {
+                return index;
             }
         }
         return 0;
