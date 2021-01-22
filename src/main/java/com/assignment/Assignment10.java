08cf3b9420c430416f2a50b0cdc485b8574cc159
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-22 17:58:30 +0530

    commit 2

--- src/main/java/com/assignment/Assignment10.java
+++ src/main/java/com/assignment/Assignment10.java
@@ -3,27 +3,14 @@ package com.assignment;
 import java.util.ArrayList;
 
 public class Assignment10 {
-    public ArrayList getOddNumbers(int min, int max) {
 
+    public ArrayList getOddNumbers(int min, int max) {
         ArrayList<Integer> oddNumbersArraylist = new ArrayList<>();
-        int index=max+min;
-        int numberArray[]=new int[index];
         for (int i = min; i <= max; i++) {
             if (i % 2 == 1) {
                 oddNumbersArraylist.add(i);
             }
         }
-        /*for (int i : numberArray) {
-            numberArray[i]=i;
-            if (i % 2 == 1) {
-                oddNumbersArraylist.add(i);
-            }
-        }*/
-        /*for (index:numberArray) {
-            if (index % 2 == 1) {
-                oddNumbersArraylist.add(index);
-            }
-        }*/
         return oddNumbersArraylist;
     }
 }
--- src/test/java/com/assignmentTest/Assignment10Test.java
+++ src/test/java/com/assignmentTest/Assignment10Test.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -15,22 +14,22 @@ public class Assignment10Test {
     @Test
     public void testPrintOdd() {
         Assignment10 assignment10 = new Assignment10();
-        int min = 1;
-        int max = 10;
+        int min = 1, max = 10;
         ArrayList oddNumbersArray = assignment10.getOddNumbers(min, max);
         Iterator iterator = oddNumbersArray.iterator();
         while (iterator.hasNext()) {
             logger.info((iterator.next()).toString());
         }
 
-        //Assertion to check if odd numbers are retrieved correctly
+        /**
+         * Assertion to check if odd numbers are retrieved correctly
+         */
         ArrayList<Integer> oddNumbersVerfication = new ArrayList<>();
         for (int i = min; i <= max; i++) {
             if (i % 2 == 1) {
                 oddNumbersVerfication.add(i);
             }
         }
-
         Assertions.assertArrayEquals(oddNumbersArray.toArray(), oddNumbersVerfication.toArray(), "Doesnt Match");
     }
 }
