a8adc0840d77da7d719cf0914cdbfe040b63bf7c
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-22 19:21:59 +0530

    commit 12

--- src/test/java/com/assignmentTest/Assignment8Test.java
+++ src/test/java/com/assignmentTest/Assignment8Test.java
@@ -11,9 +11,11 @@ public class Assignment8Test {
 
     @Test
     public void testMiddleNumber() {
-        int lowerNumber = 1 , higherNumber = 500;
+        int lowerNumber = 1, higherNumber = 500;
         Assignment8 assignment8 = new Assignment8();
+
         logger.info(Integer.toString(assignment8.getMiddleNumber(lowerNumber, higherNumber)));
-        Assertions.assertEquals(assignment8.getMiddleNumber(lowerNumber, higherNumber), 250, "It's not the middle number");
+        Assertions.assertEquals(assignment8.getMiddleNumber(lowerNumber, higherNumber), 250,
+                "It's not the middle number");
     }
 }
