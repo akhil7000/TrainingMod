df6970fd7f5db1fd32218de30037e2d0d1c8a5f0
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-22 17:20:45 +0530

    draft

--- src/main/java/com/assignment/Assignment11.java
+++ src/main/java/com/assignment/Assignment11.java
@@ -1,4 +1,10 @@
 package com.assignment;
 
 public class Assignment11 {
+            for(int i=0;i<sport.length;i++)
+    {
+        if(i!=2){
+            System.out.println(sport[i]);
+        }
+    }
 }
