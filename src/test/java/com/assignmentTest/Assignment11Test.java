198202b0411a90fc9ed833e5ebbfd737e68dc1dd
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-22 19:33:14 +0530

    draft

--- src/test/java/com/assignmentTest/Assignment11Test.java
+++ src/test/java/com/assignmentTest/Assignment11Test.java
@@ -1,6 +1,7 @@
 package com.assignmentTest;
 
 import com.assignment.Assignment11;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,12 +14,13 @@ public class Assignment11Test {
 
     @Test
     public void testSportWithoutIndexAt(){
-        ArrayList<String> sportNamesArray=new ArrayList<>();
-        sportNamesArray.addAll(Arrays.asList("Tennis", "Volleyball","Cricket","Cycling","Swimming","Badminton"));
+        ArrayList<String> sportNamesList=new ArrayList<>();
+        sportNamesList.addAll(Arrays.asList("Tennis", "Volleyball","Cricket","Cycling","Swimming","Badminton"));
         int index =3;
         Assignment11 assignment11=new Assignment11();
-        ArrayList newSportNamesArray = assignment11.getSportExceptAtIndex(sportNamesArray,index);
-        Iterator iterator = newSportNamesArray.iterator();
+
+        ArrayList newSportNamesList = assignment11.getSportExceptAtIndex(sportNamesList,index);
+        Iterator iterator = newSportNamesList.iterator();
 
         while(iterator.hasNext()){
             logger.info(iterator.next().toString());
@@ -27,14 +29,12 @@ public class Assignment11Test {
         /**
          * Assertion to check if correct array has been retrieved
          */
-        ArrayList <String>sportNamesVerificationArray=new ArrayList<>();
-        for(int i=0;i<sportNamesArray.size();i++){
-            if (i!=index)
-            {
-                sportNamesVerificationArray.add(sportNamesArray.get(i));
-            }
-        }
-        System.out.println(sportNamesVerificationArray);
+        ArrayList <String>sportNamesVerificationList=new ArrayList<>();
+        sportNamesVerificationList = sportNamesList;
+        sportNamesVerificationList.remove(index);
+
+        Assertions.assertArrayEquals(newSportNamesList.toArray(),sportNamesVerificationList.toArray(),
+                "Arraylists don't match");
 
     }
 }
