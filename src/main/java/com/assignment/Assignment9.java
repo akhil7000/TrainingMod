69c67e88d2eb05d3a2501052e580b5d2fe7c71a5
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-22 22:35:18 +0530

    Commit 10

--- src/main/java/com/assignment/Assignment9.java
+++ src/main/java/com/assignment/Assignment9.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Assignment9 {
 
-    public ArrayList getEvenNumber(int min, int max) {
+    public ArrayList<Integer> getEvenNumber(int min, int max) {
         ArrayList<Integer> evenNumberList = new ArrayList<>();
         while (min <= max) {
             if (min % 2 == 0) {
--- src/test/java/com/assignmentTest/Assignment9Test.java
+++ src/test/java/com/assignmentTest/Assignment9Test.java
@@ -14,8 +14,7 @@ public class Assignment9Test {
     @Test
     public void testEvenNumbers() {
         int min = 1, max = 500;
-        ArrayList<Integer> evenNumbersVerification = new ArrayList<>();
-        ArrayList evenNumbers = new Assignment9().getEvenNumber(min, max);
+        ArrayList<Integer> evenNumbers = new Assignment9().getEvenNumber(min, max);
         Iterator iterator = evenNumbers.iterator();
 
         while (iterator.hasNext()) {
@@ -25,11 +24,10 @@ public class Assignment9Test {
         /**
          * Assertion to check if even numbers are retrieved
          */
-        for (int i = min; i <= max; i++) {
-            if (i % 2 == 0) {
-                evenNumbersVerification.add(i);
-            }
+
+        for (int i : evenNumbers) {
+            Assertions.assertTrue(i % 2 == 0, "Not Even");
         }
-        Assertions.assertArrayEquals(evenNumbers.toArray(), evenNumbersVerification.toArray(), "Doesn't Match");
+
     }
 }
