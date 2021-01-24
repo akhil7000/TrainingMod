9b82fed08ecb90e57078d85196d63b0d77af1285
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-24 16:03:33 +0530

    Commit 2

---
+++ src/main/java/com/assignment/StudentAssignment12.java
@@ -0,0 +1,41 @@
+package com.assignment;
+
+public class StudentAssignment12 {
+    String name;
+    String lastname;
+    int rollno;
+    String school = "PMMIWS";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = "Barrack";
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = "Obama";
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = 1;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = "PMMIWS";
+    }
+
+}
--- src/test/java/com/AssignmentTest/Assignment12Test.java
+++ src/test/java/com/AssignmentTest/Assignment12Test.java
@@ -1,18 +1,31 @@
 package com.AssignmentTest;
-import com.a.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-//import com.assignment
+import com.assignment.StudentAssignment12;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Assignment12Test {
-    Student obj = new Student();
-    BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("Enter name "+"Last name "+"Roll number ");
-    obj.name = rd.readLine();
-    obj.lastname=rd.readLine();
-    obj.rollno = Integer.parseInt(rd.readLine());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Test
+    public void testStudentInfo() {
+        StudentAssignment12 studentAssignment12 = new StudentAssignment12();
+        String name = "Barrack", lastname = "Obama";
+        int rollno = 1;
+        studentAssignment12.setName(name);
+        studentAssignment12.setLastname(lastname);
+        studentAssignment12.setRollno(1);
+        logger.info("First Name: " + studentAssignment12.getName());
+        logger.info("Lastname:" + studentAssignment12.getLastname());
+        logger.info("Roll NO: " + studentAssignment12.getRollno());
+        logger.info("School: " + studentAssignment12.getSchool());
+        Assertions.assertTrue(studentAssignment12.getName() == name, "Name doesn't match");
+        Assertions.assertTrue(studentAssignment12.getLastname() == lastname, "Lastname doesn't match");
+        Assertions.assertTrue(studentAssignment12.getRollno() == rollno, "Roll No Doesn't Match");
+        Assertions.assertTrue(studentAssignment12.getSchool() == "PMMIWS", "School Name Doesn't match");
+    }
 }
 
 
