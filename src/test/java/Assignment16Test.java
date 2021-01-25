39167c1ba87e08e94707839d282ed603daf33a00
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-01-25 16:29:45 +0530

    commit 7

--- src/test/java/Assignment16Test.java
+++ src/test/java/Assignment16Test.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.assignment.*;
 
 public class Assignment16Test {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -12,6 +11,7 @@ public class Assignment16Test {
     public void testAdditionUsingAbstract() {
         int integerToSum = 1;
         AdditionOfNumbers additionOfNumbers = new AdditionOfNumbers();
+
         logger.info(Integer.toString(additionOfNumbers.getSum(integerToSum)));
         Assertions.assertEquals(additionOfNumbers.getSum(integerToSum), 3,
                 "Sum is not correct");
