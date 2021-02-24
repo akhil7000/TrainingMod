d2358c6fbfe349a4adb79f0dbc4e45aa2675b6de
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-02-25 00:38:19 +0530

    commit 5

--- src/main/java/com/training/basepages/FlipkartBasePage.java
+++ src/main/java/com/training/basepages/FlipkartBasePage.java
@@ -3,14 +3,19 @@ import com.training.web.pages.flipkart.FlipkartHomePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-public class FlipkartBasePage extends FlipkartHomePage{
+public class FlipkartBasePage{
+    public WebDriver driver;
+    public WebDriverWait wait;
+
     private By loaderIcon = By.xpath("//div[@class='_2YsvKq _3bgaUQ']/*[name()='svg']");
     private By cartIcon = By.xpath("//*[text()='Cart']");
 
     public FlipkartBasePage(WebDriver driver) {
-        super(driver);
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 30);
     }
 
     public void clickCartIcon() {
