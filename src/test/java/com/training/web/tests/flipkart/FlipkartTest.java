568bacb39584b3ceca6881f7309443ffc0a5a346
Pranav <hanwantepranav@gmail.com>
2021-04-15 00:17:54 +0530

    sixth committtt

--- src/test/java/com/training/web/tests/flipkart/FlipkartTest.java
+++ src/test/java/com/training/web/tests/flipkart/FlipkartTest.java
@@ -138,5 +138,6 @@ public class FlipkartTest extends WebBaseTest {
         System.out.println("4");
         System.out.println("5");
         System.out.println("6");
+        System.out.println("7");
     }
 }
