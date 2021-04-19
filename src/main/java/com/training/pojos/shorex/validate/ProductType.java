0092eafe677a149e9b3c6880af7942fa8aac5b51
dheerajsrinath <dheeraj.srinath@outlook.com>
2021-04-19 20:36:54 +0530

    Test for Shorex Product Type

---
+++ src/main/java/com/training/pojos/shorex/validate/ProductType.java
@@ -0,0 +1,10 @@
+package com.training.pojos.shorex.validate;
+
+import lombok.Data;
+
+@Data
+public class ProductType {
+    private String productTypeID;
+    private String productTypeName;
+    private String productType;
+}
---
+++ src/main/java/com/training/pojos/shorex/validate/Products.java
@@ -0,0 +1,8 @@
+package com.training.pojos.shorex.validate;
+
+import lombok.Data;
+
+@Data
+public class Products {
+    private ProductType productType;
+}
