faa00265eba7a20e04a675ba2447e515220bf3e7
unnikrishnanks-1 <unnikrishnanks1234@gmail.com>
2021-06-11 17:31:04 +0530

    first commit

---
+++ src/main/java/com/training/sample/pages/Assignment1.java
@@ -0,0 +1,41 @@
+package com.training.sample.pages;
+
+public class Assignment1 {
+    public static double intToDouble(int x) {
+        double d = (double) x;
+        System.out.println(d);
+        return d;
+    }
+
+    public static int doubleToInt(double d) {
+        int x = (int) d;
+        System.out.println(x);
+        return x;
+    }
+
+    public static float doubleToFloat(double d) {
+        float f = (float) d;
+        System.out.println(f);
+        return f;
+    }
+
+    public static double floatToDouble(float f) {
+        double d = (double) f;
+        System.out.println(d);
+        return d;
+    }
+    public static long byteToLong(byte b) {
+        long l = (long) b;
+        System.out.println(l);
+        return l;
+    }
+    public static byte longToByte(long L){
+        byte b = (byte) L;
+        System.out.println(b);
+        return b;
+    }
+    public static char[] stringToCharArray(String s){
+        char[] characters=s.toCharArray();
+        return characters;
+    }
+}
---
+++ src/test/java/com/training/sample/tests/Assignment1Test.java
@@ -0,0 +1,47 @@
+package com.training.sample.tests;
+
+import com.training.sample.pages.Assignment1;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Assignment1Test {
+    Assignment1 a1=new Assignment1();
+    @Test
+    public void intToDoubleTest(){
+        double result=a1.intToDouble(200);
+        Assertions.assertEquals(200.0,result);
+    }
+    @Test
+    public void doubleToInt(){
+        int result=a1.doubleToInt(200.0);
+        Assertions.assertEquals(200,result);
+    }
+    @Test
+    public void doubleToFloat(){
+        float result=a1.doubleToFloat(55.005001068115234);
+        Assertions.assertEquals(55.005f,result);
+    }
+    @Test
+    public void floatToDouble(){
+        double result=a1.floatToDouble(55.005f);
+        Assertions.assertEquals(55.005001068115234,result);
+    }
+    @Test
+    public void byteToLong(){
+        int x=-128;
+        byte b=(byte)x;
+        long result=a1.byteToLong(b);
+        Assertions.assertEquals(-128,result);
+    }
+    @Test
+    public void longToByte(){
+        byte result=a1.longToByte(59863248L);
+        Assertions.assertEquals(-48,result);
+    }
+    @Test
+    public void stringToCharArray(){
+        char[] exp ={'r','c','c','l'};
+        char[] result=a1.stringToCharArray("rccl");
+        Assertions.assertArrayEquals(exp,result);
+    }
+}
