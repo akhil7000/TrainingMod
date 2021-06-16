f16e22b76f476cc58f1841fbe30a5c93072afc2c
unnikrishnanks-1 <unnikrishnanks1234@gmail.com>
2021-06-16 20:44:01 +0530

    camelcasing commit

--- src/test/java/com/training/sample/tests/Assignment1Test.java
+++ src/test/java/com/training/sample/tests/Assignment1Test.java
@@ -8,39 +8,39 @@ public class Assignment1Test {
     Assignment1 assignment1 = new Assignment1();
 
     @Test
-    public void testintToDouble() {
+    public void testIntToDouble() {
         Assertions.assertEquals(200.0, assignment1.intToDouble(200));
     }
 
     @Test
-    public void testdoubleToInt() {
+    public void testDoubleToInt() {
         Assertions.assertEquals(200, assignment1.doubleToInt(200.0));
     }
 
     @Test
-    public void testdoubleToFloat() {
+    public void testDoubleToFloat() {
         Assertions.assertEquals(55.005f, assignment1.doubleToFloat(55.005001068115234));
     }
 
     @Test
-    public void testfloatToDouble() {
+    public void testFloatToDouble() {
         Assertions.assertEquals(55.005001068115234, assignment1.floatToDouble(55.005f));
     }
 
     @Test
-    public void testbyteToLong() {
+    public void testByteToLong() {
         int x = -128;
         byte b = (byte) x;
         Assertions.assertEquals(-128, assignment1.byteToLong(b));
     }
 
     @Test
-    public void testlongToByte() {
+    public void testLongToByte() {
         Assertions.assertEquals(-48, assignment1.longToByte(59863248L));
     }
 
     @Test
-    public void teststringToCharArray() {
+    public void testStringToCharArray() {
         char[] exp = {'r', 'c', 'c', 'l'};
         Assertions.assertArrayEquals(exp, assignment1.stringToCharArray("rccl"));
     }
