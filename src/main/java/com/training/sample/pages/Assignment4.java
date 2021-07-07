660fb5d747a015e2f40277495416e6a69957dc44
unnikrishnanks-1 <unnikrishnanks1234@gmail.com>
2021-07-07 19:40:17 +0530

    camelcasing variables commit

--- src/main/java/com/training/sample/pages/Assignment4.java
+++ src/main/java/com/training/sample/pages/Assignment4.java
@@ -1,7 +1,7 @@
 package com.training.sample.pages;
 
 public class Assignment4 {
-    public String getString(String sentence, int startindex, int endindex) {
-        return sentence.substring(startindex, endindex);
+    public String getString(String sentence, int startIndex, int endIndex) {
+        return sentence.substring(startIndex, endIndex);
     }
 }
