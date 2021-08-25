1ee75ae5e8ff1ccbb389808e4a0554e218fec31f
unnikrishnanks-1 <unnikrishnanks1234@gmail.com>
2021-08-25 17:05:04 +0530

    initialcode commit

---
+++ src/main/java/com/training/sample/pages/Assignment5.java
@@ -0,0 +1,13 @@
+package com.training.sample.pages;
+
+public class Assignment5 {
+    public int getString(String[] names, String word) {
+        int index = 0;
+        for (int i = 0; i < names.length; i++) {
+            if (names[i] == word) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
---
+++ src/test/java/com/training/sample/tests/Assignment5Test.java
@@ -0,0 +1,15 @@
+package com.training.sample.tests;
+
+import com.training.sample.pages.Assignment5;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Assignment5Test {
+    Assignment5 assignment5 = new Assignment5();
+
+    @Test
+    public void testGetString() {
+        String[] names = {"Paulo Dybala", "Federico", "Gianluigi", "Ronaldo", "Messi"};
+        Assertions.assertEquals(3, assignment5.getString(names, "Ronaldo"));
+    }
+}
