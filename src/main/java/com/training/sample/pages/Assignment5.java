429e8e3ec4c04a649c7ed2b3edbe36a47bbfcee0
unnikrishnanks-1 <unnikrishnanks1234@gmail.com>
2021-09-02 22:38:09 +0530

    exception commit

--- src/main/java/com/training/sample/pages/Assignment5.java
+++ src/main/java/com/training/sample/pages/Assignment5.java
@@ -2,10 +2,14 @@ package com.training.sample.pages;
 
 public class Assignment5 {
     public int getStringIndex(String[] names, String word) {
-        int index = 0;
+        int index = -1;
         for (int i = 0; i < names.length; i++) {
             if (names[i] == word) {
                 index = i;
+            }
+            try {
+                if (index == -1) throw new Exception();
+            } catch (Exception e) {
             }
         }
         return index;
