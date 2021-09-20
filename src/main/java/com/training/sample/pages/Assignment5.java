6dec2965182296ea629431651c54f1697de31ef8
unnikrishnanks-1 <unnikrishnanks1234@gmail.com>
2021-09-20 19:58:21 +0530

    removing index variable commit

--- src/main/java/com/training/sample/pages/Assignment5.java
+++ src/main/java/com/training/sample/pages/Assignment5.java
@@ -2,10 +2,9 @@ package com.training.sample.pages;
 
 public class Assignment5 {
     public int getStringIndex(String[] names, String word) throws Exception {
-        int index = -1;
         for (int i = 0; i < names.length; i++) {
             if (names[i].equalsIgnoreCase(word)) {
-                return index = i;
+                return i;
             }
         }
         throw new Exception("Element match not found");
