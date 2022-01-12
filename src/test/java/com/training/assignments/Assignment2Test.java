0e12f0a807714a4f40a53b13974a9c957d40a890
naviya <d.naviya@gmail.com>
2022-01-12 12:23:16 -0500

    Assignment_2 resolved code review changes

--- src/test/java/com/training/assignments/Assignment2Test.java
+++ src/test/java/com/training/assignments/Assignment2Test.java
@@ -9,17 +9,17 @@ class Assignment2Test {
     Assignment2 assignment2 = new Assignment2();
 
     @Test
-    public void string1Test() {
+    public void testString1() {
         Assertions.assertEquals("Sam", assignment2.string1("Ram", "Sam"));
     }
 
     @Test
-    public void string2Test() {
+    public void testString2() {
         Assertions.assertEquals("Sachin Masterrrr", assignment2.string1("Sachin Tendulkar", "Sachin Masterrrr"));
     }
 
     @Test
-    public void string3Test() {
+    public void testString3() {
         Assertions.assertEquals("Depp", assignment2.string1("Dell", "Depp"));
     }
 }
