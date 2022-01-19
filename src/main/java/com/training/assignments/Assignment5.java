352cd252c416621a620773919dfc430fdf3ba80f
naviya <d.naviya@gmail.com>
2022-01-19 16:11:51 -0500

    Assignment_5 commit

---
+++ src/main/java/com/training/assignments/Assignment5.java
@@ -0,0 +1,12 @@
+package com.training.assignments;
+
+public class Assignment5 {
+    public int getIndexArray(String[] array, String word) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(word)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
