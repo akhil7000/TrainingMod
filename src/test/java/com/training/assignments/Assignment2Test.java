e2ba5e3a5c7d3fc978e6110016f425290198b4b1
naviya <d.naviya@gmail.com>
2022-01-19 10:37:01 -0500

    Assignment_2 made changes in import and removed extra test classes

--- src/test/java/com/training/assignments/Assignment2Test.java
+++ src/test/java/com/training/assignments/Assignment2Test.java
@@ -3,23 +3,13 @@ package com.training.assignments;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class Assignment2Test {
     Assignment2 assignment2 = new Assignment2();
 
     @Test
-    public void testString1() {
+    public void testStringReplace() {
         Assertions.assertEquals("Sam", assignment2.getStringReplace("Ram", "Sam"));
-    }
-
-    @Test
-    public void testString2() {
         Assertions.assertEquals("Sachin Masterrrr", assignment2.getStringReplace("Sachin Tendulkar", "Sachin Masterrrr"));
-    }
-
-    @Test
-    public void testString3() {
         Assertions.assertEquals("Depp", assignment2.getStringReplace("Dell", "Depp"));
     }
 }
