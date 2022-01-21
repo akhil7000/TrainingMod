271a253e0aa9e30b86128322263859532567857a
naviya <d.naviya@gmail.com>
2022-01-21 13:53:31 -0500

    Assignment_5 made changes to name in test

--- src/test/java/com/training/assignments/Assignment5Test.java
+++ src/test/java/com/training/assignments/Assignment5Test.java
@@ -7,7 +7,7 @@ class Assignment5Test {
     Assignment5 assignment5 = new Assignment5();
 
     @Test
-    public void testGetIndexArray() {
+    public void testGetArrayIndex() {
         String array[] = {"Paulo Dybala", "Federico", "Gianluigi", "Ronaldo", "Messi"};
         Assertions.assertEquals(3, assignment5.getArrayIndex(array, "Ronaldo"));
         Assertions.assertEquals(-1, assignment5.getArrayIndex(array, "Ron"));
