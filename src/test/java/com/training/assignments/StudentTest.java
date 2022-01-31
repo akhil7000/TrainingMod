5d1507661a353aab3e5c07d7471766496f3b3a05
naviya <d.naviya@gmail.com>
2022-01-31 15:09:07 -0500

    Assignment_12 commit

---
+++ src/test/java/com/training/assignments/StudentTest.java
@@ -0,0 +1,26 @@
+package com.training.assignments;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    public void testStudent()
+    {
+        Student student=new Student();
+        student.setName("Naviya");
+        student.setLastName("Dayanand");
+        student.setRollNo(1);
+        Student.setSchoolName("CJCS");
+
+        Assertions.assertEquals("Naviya",student.getName());
+        Assertions.assertEquals("Dayanand",student.getLastName());
+        Assertions.assertEquals(1,student.getRollNo());
+        Assertions.assertEquals("CJCS",Student.getSchoolName());
+
+    }
+
+}
