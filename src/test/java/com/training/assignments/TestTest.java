baac99a505cd2f574423a6eaefb53a6d73eed8fd
naviya <d.naviya@gmail.com>
2022-02-03 17:13:49 -0500

    Assignment_13 added error message and made required changes

--- src/test/java/com/training/assignments/TestTest.java
+++ src/test/java/com/training/assignments/TestTest.java
@@ -3,10 +3,9 @@ package com.training.assignments;
 import org.junit.jupiter.api.Assertions;
 
 class TestTest {
-    Test test = new Test();
 
     @org.junit.jupiter.api.Test
     public void testGetFullName() {
-        Assertions.assertEquals("TYSON MIKE", test.getFullName());
+        Assertions.assertEquals("TYSON MIKE", new Test().getFullName(),"Incorrect name");
     }
 }
