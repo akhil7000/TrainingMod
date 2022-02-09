07c4e436f347479af6c6ba378cb1680d12462042
naviya <d.naviya@gmail.com>
2022-02-09 14:32:12 -0500

    Assignment_16 commit

---
+++ src/main/java/com/training/assignments/WillDoEverything.java
@@ -0,0 +1,5 @@
+package com.training.assignments;
+
+abstract public class WillDoEverything {
+    public abstract int getSum(int a);
+}
---
+++ src/main/java/com/training/assignments/Yes1.java
@@ -0,0 +1,7 @@
+package com.training.assignments;
+
+class Yes1 extends WillDoEverything {
+    public int getSum(int a) {
+        return a+a+a;
+    }
+}
